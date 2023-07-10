e3bc6c534c3fbcff015e93d02b898d6cc1bdb7d9
Zokirov Hojiakbar <136844394+khojiakbardev@users.noreply.github.com>
2023-07-10 13:46:00 +0500

    Add Program file

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * LettCode - 26
+string s = "Let's take LeetCode contest";
+Console.WriteLine(ReverseWords(s));
+
+static string ReverseWords(string s)
+{
+    string[] array = s.Split(new char[] { ' ' });
+    string answer = "";
+    for (int i = 0; i<array.Length; i++)
+    {
+        for (int j = array[i].Length - 1; j >= 0; j--)
+        {
+            answer += array[i][j];
+        }
+        if(i== array.Length - 1)
+        answer += "" ;
+    }
+
+    return answer;
+}
+*/
+/*
+            int num;
+            Console.WriteLine("100 dan 199 gacha bo'lgan oraliqda sonlarni kirit:");
+            num = int.Parse(Console.ReadLine());
+            if (num < 100 || num > 199)
+            {
+                Console.WriteLine("Faqat 100 dan 199 gacha oraliqda sonlarni kirit.");
+                return;
+            }
+            string[] units = { "","bir", "ikki", "uch", "to'rt", "besh", "olti", "yetti", "sakkiz", "to'qqiz" };
+            string[] tens = { "" ,"O`n", "Yigirma", "O`ttiz", "Qirq", "Ellik", "Otmish", "Yetmish", "Sakson", "To`qson" };
+            int hundreds = num / 100;
+            int remainder = num % 100;
+            Console.Write(units[hundreds] + " yuz");
+            if (remainder > 0)
+            {
+                Console.Write(" ");
+                if (remainder < 20)
+                {
+                    Console.Write(units[remainder]);
+                }
+                else
+                {
+                    int tensDigit = remainder / 10;
+                    int unitsDigit = remainder % 10;
+                    Console.Write(tens[tensDigit] + " " + units[unitsDigit]);
+                }
+            }*/
+namespace Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        } 
+    }
+}
+
